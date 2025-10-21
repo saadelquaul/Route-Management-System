757e2f6b81af31b02ed7a6524766c6a6db874392
Saad el quaul <saadelquaul@gmail.com>
2025-10-21 11:40:12 +0100

    initializing the project using Spring boot initializer

---
+++ src/main/java/com/route_managment_system/RMS/RouteManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.route_managment_system.RMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/route_managment_system/RMS/RouteManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.route_managment_system.RMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
