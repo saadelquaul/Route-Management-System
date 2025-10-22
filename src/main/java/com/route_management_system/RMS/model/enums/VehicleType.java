c4fa85f34bc2a7513fe14a1603c4723c5fc527f8
Saad el quaul <saadelquaul@gmail.com>
2025-10-22 17:03:08 +0100

    Implementing constraints for BIKE to specify the maximum capacity

--- src/main/java/com/route_management_system/RMS/model/enums/VehicleType.java
+++ src/main/java/com/route_management_system/RMS/model/enums/VehicleType.java
@@ -1,5 +1,26 @@
 package com.route_management_system.RMS.model.enums;
 
 public enum VehicleType {
+        BIKE(50, 0.5);
+
+
+
+        private final double maxWeightKg;
+        private final double maxVolumeM3;
+
+
+        VehicleType(double maxWeightKg, double maxVolumeM3) {
+            this.maxWeightKg = maxWeightKg;
+            this.maxVolumeM3 = maxVolumeM3;
+        }
+
+        public double getMaxWeightKg() {
+            return maxWeightKg;
+        }
+
+        public double getMaxVolumeM3() {
+            return maxVolumeM3;
+        }
+
 
 }
