9cf8511b2f0165da59db9210751ac7f0b3829501
Saad el quaul <saadelquaul@gmail.com>
2025-10-22 15:50:15 +0100

    adding VehicleRepository interface

--- src/main/java/com/route_management_system/RMS/model/Vehicle.java
+++ src/main/java/com/route_management_system/RMS/model/Vehicle.java
@@ -3,23 +3,33 @@ package com.route_management_system.RMS.model;
 
 import com.route_management_system.RMS.model.enums.VehicleType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "vehicles")
 public class Vehicle {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    VehicleType vehicleType;
+    private VehicleType vehicleType;
 
-    @Column(nullable = false)
-    Double maxWeightKg;
 
-    @Column(nullable = false)
-    Double maxVolumeM3;
+    @Column(nullable = false, name = "max_weight_kg")
+    private Double maxWeightKg;
+
+    @Column(nullable = false, name = "max_volume_m3")
+    private Double maxVolumeM3;
+
+
+    @OneToMany(mappedBy = "vehicle", fetch = FetchType.LAZY)
+    private List<Toure> tours;
 }
