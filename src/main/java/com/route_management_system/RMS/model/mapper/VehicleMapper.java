e9d1ad6799e32b9ec075c1e54173cae1a0fbc1bb
Saad el quaul <saadelquaul@gmail.com>
2025-10-23 15:21:40 +0100

    Add VehicleMapper class for converting between Vehicle and VehicleDTO

---
+++ src/main/java/com/route_management_system/RMS/model/mapper/VehicleMapper.java
@@ -0,0 +1,34 @@
+package com.route_management_system.RMS.model.mapper;
+
+import com.route_management_system.RMS.model.Vehicle;
+import com.route_management_system.RMS.model.dto.VehicleDTO;
+
+public class VehicleMapper {
+
+    public VehicleDTO toDTO(Vehicle vehicle) {
+        if(vehicle == null) {
+            return null;
+        }
+
+        VehicleDTO dto = new VehicleDTO();
+        dto.setId(vehicle.getId());
+        dto.setType(vehicle.getVehicleType());
+        dto.setMaxWeightKg(vehicle.getMaxWeightKg());
+        dto.setMaxVolumeM3(vehicle.getMaxVolumeM3());
+
+        return dto;
+    }
+
+    public Vehicle toEntity(VehicleDTO dto) {
+        if(dto == null) {
+            return null;
+        }
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.setId(dto.getId());
+        vehicle.setVehicleType(dto.getType());
+        vehicle.setMaxWeightKg(dto.getMaxWeightKg());
+        vehicle.setMaxVolumeM3(dto.getMaxVolumeM3());
+        return vehicle;
+    }
+}
