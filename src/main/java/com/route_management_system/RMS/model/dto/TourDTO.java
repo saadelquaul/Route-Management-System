ba2ec9d23ab0fb2a6e48b0787c96cd952cb3b3db
Saad el quaul <saadelquaul@gmail.com>
2025-10-24 11:19:36 +0100

    Add TourDTO class with fields for tour details, vehicle and warehouse associations, and delivery list

---
+++ src/main/java/com/route_management_system/RMS/model/dto/TourDTO.java
@@ -0,0 +1,24 @@
+package com.route_management_system.RMS.model.dto;
+
+
+
+import com.route_management_system.RMS.model.enums.OptimizationAlgorithmType;
+import com.route_management_system.RMS.model.enums.TourStatus;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class TourDTO {
+
+    private Long id ;
+    private Long vehicleId;
+    private Long warehouseId;
+    private TourStatus status;
+    private OptimizationAlgorithmType algorithmType;
+    private Double totalDistanceKm;
+
+    private List<DeliveryDTO> deliveries;
+}
