289f5c4617e9b9aaf46dc9069402b08c017be53c
Saad el quaul <saadelquaul@gmail.com>
2025-10-25 17:46:15 +0100

    Add distanceFromWarehouse field to Delivery entity

--- src/main/java/com/route_management_system/RMS/model/Delivery.java
+++ src/main/java/com/route_management_system/RMS/model/Delivery.java
@@ -33,6 +33,9 @@ public class Delivery {
     @Column(name = "volume_m3", nullable = false)
     private Double volumeM3;
 
+    @Column(name = "distance_from_warehouse", nullable = false)
+    private double distanceFromWarehouse;
+
     @Column(name = "preferred_time_start")
     private LocalTime preferredDeliveryTimeStart;
 
