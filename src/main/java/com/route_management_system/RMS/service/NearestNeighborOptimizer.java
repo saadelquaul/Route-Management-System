5bdf3cd05ed7fcf71895171b40019df6b1b543b2
Saad el quaul <saadelquaul@gmail.com>
2025-10-25 18:06:55 +0100

    Remove unused distance calculation method from NearestNeighborOptimizer to be added to TourService

--- src/main/java/com/route_management_system/RMS/service/NearestNeighborOptimizer.java
+++ src/main/java/com/route_management_system/RMS/service/NearestNeighborOptimizer.java
@@ -56,18 +56,4 @@ public class NearestNeighborOptimizer implements TourOptimizer{
         return tour;
 
     }
-
-    public double calculateDistance(double warehouseLatitude, double warehouseLongitude, double deliveryLatitude, double deliveryLongitude) {
-        final int R = 6371;
-
-        double latDistance = Math.toRadians(deliveryLatitude - warehouseLatitude);
-        double lonDistance = Math.toRadians(deliveryLongitude - warehouseLongitude);
-
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(warehouseLatitude)) * Math.cos(Math.toRadians(deliveryLatitude))
-                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-        return R * c;
-    }
 }
