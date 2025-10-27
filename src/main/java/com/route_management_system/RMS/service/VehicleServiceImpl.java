0ceb755e7c654ade8e037f5c600fbceb536180c0
Saad el quaul <saadelquaul@gmail.com>
2025-10-27 11:17:16 +0100

    Implement VehicleServiceImpl with CRUD methods for vehicle management

---
+++ src/main/java/com/route_management_system/RMS/service/VehicleServiceImpl.java
@@ -0,0 +1,27 @@
+package com.route_management_system.RMS.service;
+
+import com.route_management_system.RMS.model.dto.VehicleDTO;
+
+import java.util.List;
+
+public class VehicleServiceImpl implements VehicleService{
+    @Override
+    public VehicleDTO createVehicle(VehicleDTO vehicleDTO) {
+        return null;
+    }
+
+    @Override
+    public VehicleDTO getVehicleById(Long vehicleId) {
+        return null;
+    }
+
+    @Override
+    public List<VehicleDTO> getAllVehicles() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteVehicle(Long vehicleId) {
+
+    }
+}
