a91b9d06fa183a7723f92552003666957e90cc25
Saad el quaul <saadelquaul@gmail.com>
2025-10-27 11:17:56 +0100

    Add VehicleMapper and VehicleRepository dependencies to VehicleServiceImpl

--- src/main/java/com/route_management_system/RMS/service/VehicleServiceImpl.java
+++ src/main/java/com/route_management_system/RMS/service/VehicleServiceImpl.java
@@ -1,10 +1,17 @@
 package com.route_management_system.RMS.service;
 
 import com.route_management_system.RMS.model.dto.VehicleDTO;
+import com.route_management_system.RMS.model.mapper.VehicleMapper;
+import com.route_management_system.RMS.repository.VehicleRepository;
+import lombok.Setter;
 
 import java.util.List;
 
+@Setter
 public class VehicleServiceImpl implements VehicleService{
+
+    VehicleMapper vehicleMapper;
+    VehicleRepository vehicleRepository;
     @Override
     public VehicleDTO createVehicle(VehicleDTO vehicleDTO) {
         return null;
