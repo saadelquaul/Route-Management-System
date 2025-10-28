d2a5585490390b0e35e351d0e5e2bef039b723c4
Saad el quaul <saadelquaul@gmail.com>
2025-10-28 15:01:11 +0100

    Update findWarehouseByLongitudeAndLatitude method to use double parameters for longitude and latitude

--- src/main/java/com/route_management_system/RMS/repository/WarehouseRepository.java
+++ src/main/java/com/route_management_system/RMS/repository/WarehouseRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
 
-    Optional<Warehouse> findWarehouseByLongitudeAndLatitude(String longitude, String latitude);
+    Optional<Warehouse> findWarehouseByLongitudeAndLatitude(double longitude, double latitude);
 }
