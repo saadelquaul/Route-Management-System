4f3f031bb7c80f0328a1f75dbf44771777f7b337
Saad el quaul <saadelquaul@gmail.com>
2025-10-30 16:08:29 +0100

    Refactor TourController to streamline tour creation, retrieval, and deletion endpoints

--- src/main/java/com/route_management_system/RMS/controller/TourController.java
+++ src/main/java/com/route_management_system/RMS/controller/TourController.java
@@ -1,30 +1,48 @@
 package com.route_management_system.RMS.controller;
 
 
+import com.route_management_system.RMS.model.Tour;
 import com.route_management_system.RMS.model.dto.TourDTO;
-import com.route_management_system.RMS.model.enums.OptimizationAlgorithmType;
 import com.route_management_system.RMS.service.TourService;
 import lombok.Setter;
-import org.apache.coyote.Request;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/tours")
+@RequestMapping("/api/tour")
 @Setter
 public class TourController {
 
     private TourService tourService;
 
-    @PostMapping("/optimize")
-    public ResponseEntity<TourDTO> optimizedTour(@RequestBody TourDTO tourDTO) {
+    @PostMapping
+    public ResponseEntity<TourDTO> createTour(@RequestBody TourDTO tourDTO) {
 
-        TourDTO optimizedTour = tourService.getOptimizedTour(tourDTO.getWarehouseId(), tourDTO.getVehicleId());
-        return new ResponseEntity<>(optimizedTour, HttpStatus.CREATED);
+        String algorithm = tourDTO.getAlgorithmUsed().toString();
+        if(algorithm == null) algorithm = "";
+        TourDTO tour = tourService.getOptimizedTour(tourDTO.getWarehouseId(), tourDTO.getVehicleId(),algorithm);
+        return new ResponseEntity<>(tour, HttpStatus.CREATED);
     }
+
+    @GetMapping("/")
+    public List<Tour> getAll(){
+            return tourService.getAllTours();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        tourService.deleteTour(id);
+        return  ResponseEntity.ok("tour deleted ");
+    }
+
+    @GetMapping("total/{id}")
+    public  ResponseEntity<?> totalDistance(@PathVariable Long id){
+        Double distance = tourService.getTotalDistance(id);
+        return ResponseEntity.ok(distance);
+    }
+
+
 }
