7c9cca9b56484ca2957532c5ba9ec8541421f16e
Saad el quaul <saadelquaul@gmail.com>
2025-10-30 16:08:49 +0100

    Update TourService interface to include algorithm name in getOptimizedTour method signature

--- src/main/java/com/route_management_system/RMS/service/TourService.java
+++ src/main/java/com/route_management_system/RMS/service/TourService.java
@@ -1,17 +1,15 @@
 package com.route_management_system.RMS.service;
 
-import com.route_management_system.RMS.model.Delivery;
+
 import com.route_management_system.RMS.model.Tour;
-import com.route_management_system.RMS.model.Vehicle;
-import com.route_management_system.RMS.model.Warehouse;
+
 import com.route_management_system.RMS.model.dto.TourDTO;
 
 import java.util.List;
 
 public interface TourService {
 
-    double calculateDistance(double warehouseLatitude, double warehouseLongitude, double deliveryLatitude, double deliveryLongitude);
-    TourDTO getOptimizedTour(Long warehouseId, Long vehicleId);
+    TourDTO getOptimizedTour(Long warehouseId, Long vehicleId, String algorithmUsedName);
     double getTotalDistance(Long tourId);
     TourDTO findById(Long tourId);
     boolean deleteTour(Long tourId);
